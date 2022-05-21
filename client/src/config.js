378a2b0106c1043ae1b74314fe97ff0f4662a15e
Laurentiu Capatina <lcapatina@mantu.com>
2022-05-22 00:38:39 +0200

    for now config.js is not used

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-const config = { VUE_APP_ENV_BACKEND: 'http://localhost:5000' };
+const config = { VUE_APP_ENV_BACKEND: `http://${process.env.FLASK_SERVICE_SERVICE_HOST}:${process.env.FLASK_SERVICE_SERVICE_PORT}` };
 export default config;
