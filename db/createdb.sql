4769dcb0bddedb04c7f132eaab1a07645b50dbee
Laurentiu Capatina <lcapatina@mantu.com>
2022-05-22 00:36:09 +0200

    backend connected to db

--- db/createdb.sql
+++ db/createdb.sql
@@ -1,1 +1,14 @@
 CREATE DATABASE books;
+
+USE books;
+
+CREATE TABLE books(
+    id serial PRIMARY KEY, 
+    title VARCHAR(100) UNIQUE NOT NULL,
+    author VARCHAR(100) UNIQUE NOT NULL,
+    read BOOLEAN NOT NULL
+    );
+
+INSERT INTO books(title, author, read) VALUES('On the Road', 'Jack Kerouac', TRUE);
+INSERT INTO books(title, author, read) VALUES('Harry Potter and the Philosopher''s Stone', 'J. K. Rowling', FALSE);
+INSERT INTO books(title, author, read) VALUES('Green Eggs and Hamm', 'Dr. Seuss', TRUE);
--- server/app.py
+++ server/app.py
@@ -12,45 +12,55 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 
 BASE_PATH = '/api'
 
-BOOKS = [
-    {
-        'id': 0,
-        'title': 'On the Road',
-        'author': 'Jack Kerouac',
-        'read': True
-    },
-    {
-        'id': 1,
-        'title': 'Harry Potter and the Philosopher\'s Stone',
-        'author': 'J. K. Rowling',
-        'read': False
-    },
-    {
-        'id': 2,
-        'title': 'Green Eggs and Hamm',
-        'author': 'Dr. Seuss',
-        'read': True
-    }
-]
+# BOOKS = [
+#     {
+#         'id': 0,
+#         'title': 'On the Road',
+#         'author': 'Jack Kerouac',
+#         'read': True
+#     },
+#     {
+#         'id': 1,
+#         'title': 'Harry Potter and the Philosopher\'s Stone',
+#         'author': 'J. K. Rowling',
+#         'read': False
+#     },
+#     {
+#         'id': 2,
+#         'title': 'Green Eggs and Hamm',
+#         'author': 'Dr. Seuss',
+#         'read': True
+#     }
+# ]
 
-host = os.environ.get("PG_HOST")
-database = "books"
+host = os.environ.get("POSTGRES_SERVICE_SERVICE_HOST")
+database = "admin"
 user = os.environ.get("PG_USER")
 password = os.environ.get("PG_PASSWORD")
-print(user)
-print(password)
-#conn = psycopg2.connect(host=host, database=database, user=user, password=password)
+conn = psycopg2.connect(host=host, database=database, user=user, password=password)
 
 @app.route(BASE_PATH + '/books', methods=['GET'])
 def all_books():
-    return jsonify({'status': 'success', 'books': BOOKS})
+    cursor = conn.cursor()
+    select_query = "select * from books"
+    cursor.execute(select_query)
+    books = cursor.fetchall()
+    result = [{"id": book[0], "title": book[1], "author": book[2], "read": book[3]} for book in books]
+    cursor.close()
+    return jsonify({'status': 'success', 'books': result})
 
 @app.route(BASE_PATH + '/book', methods=['POST'])
 def add_book():
     data = request.get_json()
-    last_id = BOOKS[len(BOOKS)-1]['id']
-    data['id'] = last_id + 1
-    BOOKS.append(data)
+    # last_id = BOOKS[len(BOOKS)-1]['id']
+    # data['id'] = last_id + 1
+    # BOOKS.append(data)
+    cursor = conn.cursor()
+    insert_query = """ INSERT INTO books (title, author, read) VALUES (%s,%s,%s)"""
+    values = (data["title"], data["author"], data["read"])
+    cursor.execute(insert_query, values)
+    conn.commit()
+    cursor.close()
     return jsonify({'status': 'success', 'message': "Book added!"});
 
 # sanity check route
